50ac363a1b1863ffba12c26da46c3b6df39a14fb
LameIkon <troelsjelsbak@hotmail.com>
2026-03-02 09:08:49 +0100

    The Movement works, Camera problems
    The movement controls do not follow the camera.

--- Assets/_Scripts/Data/PlayerData.cs
+++ Assets/_Scripts/Data/PlayerData.cs
@@ -6,12 +6,15 @@ using UnityEngine.InputSystem;
 public class PlayerData : ScriptableObject 
 {
     [SerializeField] private float _movementSpeed;
+    [SerializeField] private float _maxMovementSpeed;
 
-    [SerializeField] private float _mouseSpeed;
+    [SerializeField, Range(1, 1000)] private float _mouseSpeed;
+
 
 
     public float MovementSpeed => _movementSpeed;
-    public float MouseSpeed => _mouseSpeed;
+    public float MaxMovementSpeed => _maxMovementSpeed;
+    public float MouseSpeed => _mouseSpeed/5000f; // This is done becase the sensitivity, quickly changes.
 
 
 }
--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -13,56 +13,82 @@ namespace _Scripts
         private float _movementSpeed = 0.4f;
         private float _mouseSpeed = 1f;
         private Rigidbody _rb;
-        private CharacterController _characterController;
+        private Camera _camera;
 
         private Vector3 _moveDirection;
+        private Vector3 _rotateDirection;
 
-        private bool _canMove;
+        private bool _isMoving;
 
+		#region Inputs
         private void OnEnable()
         {
             InputReader.s_OnMoveEvent += Move;
+            InputReader.s_OnLookEvent += Rotate;
+            InputReader.s_OnUseEvent += Use;
+            InputReader.s_OnInteractEvent += Interact;
         }
 
         private void OnDisable()
         {
             InputReader.s_OnMoveEvent -= Move;
+            InputReader.s_OnLookEvent -= Rotate;
+            InputReader.s_OnUseEvent -= Use;
+            InputReader.s_OnInteractEvent -= Interact;
         }
 
-        private void Move(Vector2 dir)
-        {
-            _rb.AddForce(dir * _movementSpeed, ForceMode.Impulse);
-            //_characterController.Move(dir * Time.deltaTime);
+		private void Move(Vector2 dir)
+        { 
+            _moveDirection.x = dir.x;
+            _moveDirection.z = dir.y;
         }
+
+        private void Rotate(Vector2 dir) 
+        {
+            _rotateDirection += new Vector3(dir.x, -dir.y, 0) * _playerData.MouseSpeed;
+        }
+
+        private void Use() 
+        {
         
+        }
+
+        private void Interact() 
+        {
+        
+        
+        }
+
+        #endregion
+
+        #region Unity Methods
+
         void Start()
         {
             _mouseSpeed = _playerData.MovementSpeed;
             _movementSpeed = _playerData.MovementSpeed;
             _rb = GetComponent<Rigidbody>();
-            _characterController = GetComponent<CharacterController>();
-        }
-        
-        
-        // Update is called once per frame
-        void Update()
-        {
-            //_characterController.Move(_moveDirection * Time.deltaTime * _movementSpeed);
+            _camera = GetComponentInChildren<Camera>();
         }
 
-        public void tesst(InputAction.CallbackContext context)
+
+        void FixedUpdate()
         {
-             Vector2 value = context.ReadValue<Vector2>();
-            if (context.performed)
-            {
 
-                _moveDirection = new Vector3(value.x, 0, value.y);
-            }
+            // This makes the player move
+            _rb.AddForce(_moveDirection * _playerData.MovementSpeed);
+            
+            // This should probably be changed in the furture the movement keys do not follow the camera with this set up.
+            _camera.GetComponent<Transform>().rotation = Quaternion.Euler(_rotateDirection.y, _rotateDirection.x, 0);
 
-            if (context.canceled)
-            {
-                _moveDirection = Vector3.zero;
-            }
-        }          
-    }
+        }
+
+        private void Reset() 
+        {
+            _rb = GetComponent<Rigidbody>();
+            _rb.linearDamping = _playerData.MovementSpeed / 10; // Important for making the player stop again.
+        }
+
+		#endregion
+	}
 }
