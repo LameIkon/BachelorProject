51dce41f98a8b6c0f8e3f0b8f21bc92edef19103
LameIkon <troelsjelsbak@hotmail.com>
2026-03-23 11:24:20 +0100

    IPlaceable correct naming

--- Assets/_Scripts/Interactables/Iplaceable.cs
+++ Assets/_Scripts/Interactables/Iplaceable.cs
@@ -2,7 +2,7 @@
 /// <summary>
 /// Works together with Ipickable. An enum PickableType is used to address specific locations to drop pickables if any assigned
 /// </summary>
-public interface Iplaceable
+public interface IPlaceable
 {
     PickableType PickableType { get; }
 }
