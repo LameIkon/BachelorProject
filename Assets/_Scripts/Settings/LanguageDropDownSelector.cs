b1206395e9fe23d643333a55f58b4c2691d67033
JohanPedersen11 <Johan.munch.2002@gmail.com>
2026-04-16 15:53:50 +0200

    language selector

--- Assets/_Scripts/Settings/LanguageDropDownSelector.cs
+++ Assets/_Scripts/Settings/LanguageDropDownSelector.cs
@@ -1,10 +1,12 @@
+using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LanguageDropDownSelector : MonoBehaviour
 {
-    private Dropdown _dropdownLanguageOptions;
+    private TMP_Dropdown _dropdownLanguageOptions;
 
     private void Awake()
     {
@@ -13,18 +15,49 @@ public class LanguageDropDownSelector : MonoBehaviour
 
     private void Initialize()
     {
-        _dropdownLanguageOptions = GetComponent<Dropdown>();
+        _dropdownLanguageOptions = GetComponent<TMP_Dropdown>();
+
+        _dropdownLanguageOptions.ClearOptions();
+
+        List<TMP_Dropdown.OptionData> optionsData = new List<TMP_Dropdown.OptionData>();
+
+        foreach (Language language in Enum.GetValues(typeof(Language)))
+        {
+            TMP_Dropdown.OptionData optionData = new TMP_Dropdown.OptionData
+            {
+                text = language.ToString(),
+            };
+
+            optionsData.Add(optionData);
+        }
+
+        _dropdownLanguageOptions.AddOptions(optionsData);
 
 
-
-
+        _dropdownLanguageOptions.onValueChanged.AddListener(value => SetLanguage(value));
         
+        _dropdownLanguageOptions.captionText.text = "LanguageUtility.CurrentLanguage.ToString()";
+        ////var options = CreateOptionData(null);
+        //_dropdownLanguageOptions.AddOptions(options);
+
+
         //_dropdownLanguageOptions.AddOptions();
     }
 
     private IEnumerable<Dropdown.OptionData> CreateOptionData()
     {
+        Dropdown.OptionData optionData = new Dropdown.OptionData
+        {
+            text = string.Empty,
+        };
 
-        return null;
+        yield return optionData;
+    }
+
+    private void SetLanguage(int value)
+    {
+        Language language = (Language)value;
+
+        LanguageUtility.SetLanguage(language);
     }
 }
